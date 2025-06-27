bf5cc389956b129c5a05190015b223954b2cda70
ayu-ctrl <ayushi241006@gmail.com>
2025-06-27 19:31:53 +0530

    Add files via upload

---
+++ Variables and Data Types.py
@@ -0,0 +1,47 @@
+a=5
+b=4
+print (a**b)
+c=50
+b=20
+print (a==b) #False
+
+# num=10
+# num=num+10 #num+=10  
+
+# a=50
+# b=30
+# print (not False)
+# print (not (a<b))
+
+# val1=True
+# val2=True
+# print (val1 and val2) #and operator
+
+#type conversion 
+# a=2
+# b=4.25
+# sum=a+b #6.25
+# print (sum)
+
+# a=int(2)
+# b=4.25
+# print (type(a))
+# print (a+b)
+ 
+# name = input("enter your name:")
+# print ("Welcome", name )
+
+# val=input("age")
+# print (type(val)) #always str "25" "99.99" "Ayushi"
+
+
+val=float (input ("some value: "))
+print (type(val), val)
+
+a=int(input("a"))
+b=int(input("b"))
+print (a+b)
+
+# side = float(input("side"))
+# print (side*side)
+
