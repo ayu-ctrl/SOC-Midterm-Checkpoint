77a2c2b073d65ed2614072235af8f91080e6c04a
ayu-ctrl <ayushi241006@gmail.com>
2025-06-27 19:53:55 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,44 @@
+# loops
+#1
+i=1
+while i<5: 
+    print("hello") #infinite loop
+i=1
+while i<=5:
+    print("hello")
+    i+=1
+i=5
+while i>=1:
+    print(i)
+    i-=1
+ 
+#2
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x=36
+i=0
+while i<len(nums):
+    if(nums[i]==x):
+        print(i)
+        break
+    i+=1
+
+# break and continue
+#1
+i=1  
+while i<=5:
+    print(i)
+    if (i==3):
+        break
+    i+=1
+
+#2
+i=1
+while i<=10:
+    if(i%2==0):
+        i+=1
+        continue  #skip
+    print(i)
+    i+=1
+
+
+
